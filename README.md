b5e2fb3ebada4040ad612f74cf828c7e2f0a0f3c
Daniel Skarbek <dskarbek@stanfordalumni.org>
2019-03-06 16:08:24 -0800

    Fix bugs and add Fibonacci sample

--- README.md
+++ README.md
@@ -1,4 +1,97 @@
 # RPN-Lang
 JavaScript implementation of an interpreter for a little language based on Reverse Polish Notation (RPN)
 
-Read the comments in the RPNLang.js file to learn the syntax of the language.  Run the html file in your browser to play with the language.
+# Language
+Evaluates a reverse polish notation expression against a set of variables. The expected format is that white-space separates each token. The basic run-time elements is the stack of tokens that are being parsed and a hash-table of variables that are being referenced.
+
+## Constants
+---- | -----
+`val` | Any string that does not match an operation is interpreted as a literal value.
+`\val` | A backslash is removed from the start of a constant and the rest of the token is always taken as a constant. Use this to escape an operator to be processed as a constant.
+
+## Boolean operators
+---- | -----
+`==` | Pop two values and push true if they are equal, else push false
+`!=` | Pop two values and push false if they are equal, else push true
+`<` | Pop two values and push whether the second one is less than the top: "6 3 <" is false; "3 6 <" is true
+`>` | The following comparison operators all function similar to '<'
+`<=` |
+`>=` |
+`||` | Pop two values and push the logical OR of them
+`&&` | Pop two values and push the logical AND of them
+`!` | Pop one value and push the logical NOT of them
+
+## Numerical operators
+---- | -----
+`+` | Pop two values and push their sum
+`-` | Pop two values and subtract the first/top from the second: "3 4 -" is -1; "4 3 -" is 1
+`*` | Pop two values and push their product
+`/` | Pop two values and divide the second one by the first/top: "6 3 /" is 2; "3 6 /" is .5
+`\` | Pop two values and do integer division (round down any fractional part): "6 3 \" is 2; "3 6 \" is 0
+`%` | Pop two values and get the modulus value from division: "6 3 %" is 0; "3 6 %" is 3
+`&` | Pop two values and push a bit-wise AND of them
+`|` | Pop two values and push a bit-wise OR of them
+`^` | Pop two values and push a bit-wise XOR of them
+`~` | Pop one value and push the bit-wise NOT of it
+`<<` | Pop two values and use the top one to shift the second one left (increase value).
+`>>` | Pop two values and use the top one to shift the second one right (decrease value).
+
+## String operators
+---- | -----
+`.` | Concatenate top two values (as strings) on the stack and push the result.
+
+## Control operators
+---- | -----
+`?:` | Ternary, or if-then-else, operator: "test a b ?:" evals to `a` if `test` is true and to `b` if `test` is false.
+`()` | Pop the top of stack and evaluates it as a RPNLang expression. Typically, the top of stack should be a `{ ... }` quoted expression.
+`{ ... }` | Quotes the enclosed tokens into a single token on the stack. Can be nested. This is primarily useful for defining expressions to pass to the eval operator
+
+## Variable operators
+---- | -----
+`?` | Evaluates the top of stack token as a variable name and pushes on its value in its place.
+`=>` | This is the variable assignment operator: "5 a =>" sets a to 5.
+
+# Samples
+To help you pick up on the sort of things you can do, here's some sample programs.
+## Fibonacci
+This generates a fibonacci sequence of N values. In the sample, N is set to 10.
+```
+{
+  b =>
+  a =>
+  a ?
+  b ?
+  a ? b ? +
+} preserve_args_and_sum =>
+
+{
+  count =>
+  count ? 0 !=
+  {
+    preserve_args_and_sum ? ()
+    count ? 1 -
+    fib_safe ? ()
+  }
+  { }
+  ?: ()
+} fib_safe =>
+
+{
+  count =>
+  count ? 2 >=
+  {
+    0 1
+    count ? 2 - 
+    fib_safe ? ()
+  }
+  {
+    count ? 1 ==
+    0
+    { Invalid argument, must be called with a positive number. }
+    ?:
+  }
+  ?: ()
+} fib =>
+
+10 fib ? ()
+```
--- RPNLang.js
+++ RPNLang.js
@@ -1,61 +1,3 @@
-/**
- * Evaluates a reverse polish notation expression against a set of variables
- * The expected format is that spaces separate each token.
- *
- * Constants:
- * 		val      Any string that does not match an operation is interpretted
- * 		         as a literal value.
- * 		\val     A backslash is removed from the start of a constant and the rest of
- * 		         the token is always taken as a constant.  Use this to escape an operator
- * 		         to be processed as a constant.
- *
- * Boolean operators:
- * 		==       Pop two values and push true if they are equal, else push false
- * 		!=       Pop two values and push false if they are equal, else push true
- * 		<        Pop two values and push whether the second one is less than the top:
- * 		         "6 3 <" is false; "3 6 <" is true
- * 		>        The following comparison operators all function similar to '<'
- * 		<=
- * 		>=
- * 		||       Pop two values and push the logical OR of them
- * 		&&       Pop two values and push the logical AND of them
- * 		!        Pop one value and push the logical NOT of them
- *
- * Numerical operators:
- * 		+        Pop two values and push their sum
- * 		-        Pop two values and subtract the first/top from the second:
- * 		         "3 4 -" is -1; "4 3 -" is 1
- * 		*        Pop two values and push their product
- * 		/        Pop two values and divide the second one by the first/top:
- * 		         "6 3 /" is 2; "3 6 /" is .5
- * 		\        Pop two values and do integer division (round down any fractional part)
- * 		         "6 3 \" is 2; "3 6 \" is 0
- * 		%        Pop two values and get the modulus value from division:
- * 		         "6 3 %" is 0; "3 6 %" is 3
- * 		&        Pop two values and push a bit-wise AND of them
- * 		|        Pop two values and push a bit-wise OR of them
- * 		^        Pop two values and push a bit-wise XOR of them
- * 		~        Pop one value and push the bit-wise NOT of it
- * 		<<       Pop two values and use the top one to shift the second one left (increase value).
- * 		>>       Pop two values and use the top one to shift the second one right (decrease value).
- *
- * String operators:
- * 		.        Concatenate top two values (as strings) on the stack and push the result.
- *
- * Control operators:
- * 		?:       Ternary, or if-then-else, operator: "test a b ?:" evals to a if test is true
- * 		         and to b if test is false.
- * 		()       Pop the top of stack, look it up as a variable and evaluate the value as a RPNLang
- * 		         expression.  If it does not match a variable, then it is just discarded. It takes the
- * 		         stack as it currently is (after it is popped) and leaves it in whatever state it does.
- * 		{ .. }   Quotes the enclosed tokens into a single token on the stack.  Can be nested.  This is
- * 		         primarily useful for defining expressions to pass to the eval operator
- *
- * Variable operators:
- * 		?        Evaluates the top of stack token as a variable name and pushes on
- * 		         its value in its place.
- * 		=>       This is the variable assignment operator: "5 a =>" sets a to 5.
- */
 var RPNLang = function(vars)
 {
 	this.variables = vars
@@ -104,32 +46,32 @@ RPNLang.prototype.inner_eval = function(expr)
 			switch (token)
 			{
 				case "{" : this.stack.push(""); quoted = 1; break;
-				case "==": this.stack.push(args.pop() == args.pop()); break;
-				case "!=": this.stack.push(args.pop() != args.pop()); break;
-				case "<" : this.stack.push(args.pop() <  args.pop()); break;
-				case ">" : this.stack.push(args.pop() >  args.pop()); break;
-				case "<=": this.stack.push(args.pop() <= args.pop()); break;
-				case ">=": this.stack.push(args.pop() >= args.pop()); break;
-				case "||": this.stack.push(args.pop() || args.pop()); break;
-				case "&&": this.stack.push(args.pop() && args.pop()); break;
-				case "!" : this.stack.push( ! args.pop() ); break;
-				case "+" : this.stack.push(args.pop()*1 +  args.pop()*1); break;
-				case "-" : this.stack.push(args.pop() -  args.pop()); break;
-				case "*" : this.stack.push(args.pop() *  args.pop()); break;
-				case "/" : this.stack.push(args.pop() /  args.pop()); break;
-				case "\\": this.stack.push((args.pop() / args.pop()) | 0); break;
-				case "%" : this.stack.push(args.pop() %  args.pop()); break;
-				case "&" : this.stack.push(args.pop() &  args.pop()); break;
-				case "|" : this.stack.push(args.pop() |  args.pop()); break;
-				case "^" : this.stack.push(args.pop() ^  args.pop()); break;
-				case "~" : this.stack.push( ~ args.pop() ); break;
-				case "<<": this.stack.push(args.pop() << args.pop()); break;
-				case ">>": this.stack.push(args.pop() >> args.pop()); break;
-				case "." : this.stack.push(String(args.pop()) + args.pop()); break;
-				case "=>": this.variables[args.pop()] = args.pop(); break;
-				case "?" : this.stack.push(this.variables[args.pop()]); break;
-				case "()": this.inner_eval(args.pop()); break;
-				case "?:": this.stack.push(args.pop() == true ? args.pop() : args.shift()); break;
+				case "==": this.stack.push(args[0] == args[1]); break;
+				case "!=": this.stack.push(args[0] != args[1]); break;
+				case "<" : this.stack.push(args[0]*1 <  args[1]*1); break;
+				case ">" : this.stack.push(args[0]*1 >  args[1]*1); break;
+				case "<=": this.stack.push(args[0]*1 <= args[1]*1); break;
+				case ">=": this.stack.push(args[0]*1 >= args[1]*1); break;
+				case "||": this.stack.push(!!args[0] || !!args[1]); break;
+				case "&&": this.stack.push(!!args[0] && !!args[1]); break;
+				case "!" : this.stack.push( ! args[0] ); break;
+				case "+" : this.stack.push(args[0]*1 +  args[1]*1); break;
+				case "-" : this.stack.push(args[0] -  args[1]); break;
+				case "*" : this.stack.push(args[0] *  args[1]); break;
+				case "/" : this.stack.push(args[0] /  args[1]); break;
+				case "\\": this.stack.push((args[0] / args[1]) | 0); break;
+				case "%" : this.stack.push(args[0] %  args[1]); break;
+				case "&" : this.stack.push(args[0] &  args[1]); break;
+				case "|" : this.stack.push(args[0] |  args[1]); break;
+				case "^" : this.stack.push(args[0] ^  args[1]); break;
+				case "~" : this.stack.push( ~ args[0] ); break;
+				case "<<": this.stack.push(args[0] << args[1]); break;
+				case ">>": this.stack.push(args[0] >> args[1]); break;
+				case "." : this.stack.push(String(args[0]) + args[1]); break;
+				case "=>": this.variables[args[1]] = args[0]; break;
+				case "?" : this.stack.push(this.variables[args[0]]); break;
+				case "()": this.inner_eval(args[0]); break;
+				case "?:": this.stack.push(args[0] == true ? args[1] : args[2]); break;
 
 				default  :
 					//trim off any leading backslash used to escape the token
@@ -151,7 +93,7 @@ RPNLang.prototype.popArgs = function(token)
 	switch (token)
 	{
 	case "?:":
-		args.push(this.stack.pop());
+		args.unshift(this.stack.pop());
 	case "==":
 	case "!=":
 	case "<":
@@ -172,14 +114,14 @@ RPNLang.prototype.popArgs = function(token)
 	case "<<":
 	case ">>":
 	case ".":
-		args.push(this.stack.pop());
+		args.unshift(this.stack.pop());
 	case "!":
 	case "~":
 	case "?":
 	case "()":
-		args.push(this.stack.pop());
+		args.unshift(this.stack.pop());
 		break;
-	case "=>": //assignment we need in the opposite order
+	case "=>":
 		args.unshift(this.stack.pop());
 		args.unshift(this.stack.pop());
 		break;
